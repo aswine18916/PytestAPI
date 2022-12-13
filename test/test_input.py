54d5f81b1111e1c30267d488503aa491f34a9966
Aswin <80164936+aswine18916@users.noreply.github.com>
2022-12-13 19:38:36 +0000

    new changes

--- test/test_input.py
+++ test/test_input.py
@@ -35,21 +35,81 @@ def test_invalid_language_page():
     assert invalid_language_page.status_code, 200
 
 
-# rate a movie
-# def test_rate_movie():
-#     generate_auth_token=requests.get(getvalue("generate_auth_token"),
-#                                      params={"api_key": getvalue("api_key")}
-#                                      )
-#     # rate_a_movie=requests.post(getvalue("url")+"/977/",
-#     #                            data={
-#     #                                 "value": getvalue("movieratevalue")},
-#     #                            params={"api_key": getvalue("api_key")}
-#     #                            )
-#
-#     auth_token=generate_auth_token.json()["request_token"]
-#     print(auth_token)
-#     new_session=requests.post(getvalue("generate_new_session"),
-#                               params={"api_key": getvalue("api_key")},
-#                               data={"request_token": auth_token}
-#                               )
-#     print(new_session.json())
+#rate a movie
+def test_rate_new_movie():
+    rate_new_movie=requests.post(getvalue("url")+"/"+getvalue("movieid")+"/rating",
+                                     params={"api_key": getvalue("api_key"),
+                                             "guest_session_id": getvalue("guest_session_id")},
+                                        data={
+                                            "value": getvalue("rate_value")
+                                        }
+                                     )
+
+    assert rate_new_movie.status_code,201
+    assert rate_new_movie.json()["status_message"],"success"
+
+
+#rate an already rated movie
+def test_rate_already_rated_movie():
+    rate_already_rated_movie=requests.post(getvalue("url")+"/"+getvalue("already_rate_movie")+"/rating",
+                                     params={"api_key": getvalue("api_key"),
+                                             "guest_session_id": getvalue("guest_session_id")},
+                                        data={
+                                            "value": getvalue("rate_value")
+                                        }
+                                     )
+    # print(rate_already_rated_movie.json())
+    assert rate_already_rated_movie.status_code,201
+    assert rate_already_rated_movie.json()["status_message"],getvalue("status_already_rated_movie")
+
+
+#rate a movie with higher value
+def test_rate_movie_higher_value():
+    rate_movie_higher_value=requests.post(getvalue("url")+"/"+getvalue("already_rate_movie")+"/rating",
+                                     params={"api_key": getvalue("api_key"),
+                                             "guest_session_id": getvalue("guest_session_id")},
+                                        data={
+                                            "value": getvalue("rate_value_higher")
+                                        }
+                                     )
+    print(rate_movie_higher_value.json())
+    assert rate_movie_higher_value.json()["status_message"], getvalue("status_high_rated_movie")
+
+
+def test_rate_movie_lower_value():
+    rate_movie_lower_value=requests.post(getvalue("url")+"/"+getvalue("already_rate_movie")+"/rating",
+                                     params={"api_key": getvalue("api_key"),
+                                             "guest_session_id": getvalue("guest_session_id")},
+                                        data={
+                                            "value": getvalue("negative_rate_value")
+                                        }
+                                     )
+    # print(rate_movie_lower_value.json())
+    assert rate_movie_lower_value.json()["status_message"], getvalue("status_low_raed_movie")
+
+def test_rate_movie_invalid_api():
+    rate_movie_invalid_api=requests.post(getvalue("url")+"/"+getvalue("already_rate_movie")+"/rating",
+                                     params={"api_key": getvalue("invalid_api"),
+                                             "guest_session_id": getvalue("guest_session_id")},
+                                        data={
+                                            "value": getvalue("rate_value")
+                                        }
+                                     )
+    # print(rate_movie_invalid_api.json())
+    assert rate_movie_invalid_api.json()["status_message"], getvalue("invalid_key_status_message")
+    assert  rate_movie_invalid_api.status_code,401
+
+
+def test_rate_invalid_movie():
+    rate_invalid_movie = requests.post(getvalue("url") + "/" + getvalue("invalid_movie_id") + "/rating",
+                                   params={"api_key": getvalue("api_key"),
+                                           "guest_session_id": getvalue("guest_session_id")},
+                                   data={
+                                       "value": getvalue("rate_value")
+                                   }
+                                   )
+
+    # print(rate_invalid_movie.status_code)
+    # print(rate_invalid_movie.json())
+    assert rate_invalid_movie.status_code, 404
+    assert rate_invalid_movie.json()["status_message"], "The resource you requested could not be found."
